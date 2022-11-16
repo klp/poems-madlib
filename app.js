1fdf51d46ae985ebad5dcb97f87dc2c6b5781cf7
klp <kai.prenger@gmail.com>
2022-11-16 18:11:49 -0500

    wip

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 $("#jelly-fish").append(jellyFish);
+
+const jellyTokens = nlp(jellyFish);
+
+console.log(typeof(jellyTokens));
+
+const jellyString = JSON.stringify(jellyTokens, undefined, 4);
+
+const jellyJSON = JSON.parse(jellyString);
+
+$("#jelly-json").append(jellyJSON);
